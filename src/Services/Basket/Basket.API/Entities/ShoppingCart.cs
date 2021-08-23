9b378b20ed2de94aaa653346253384fd9d4b33f7
MustafaAkcakaya <mustafa.akcakaya@logicalbond.com>
2021-08-24 00:30:09 +0300

    Initial commit: entities, docker-compose etc. initiated.

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string Username { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart()
+        {
+        }
+        public ShoppingCart(string username)
+        {
+            Username = username;
+        }
+        public decimal TotalPrice
+        {
+            get => Items.Sum(x => x.Product.Price * x.Quantity); 
+        }
+
+    }
+
+}
---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCartItem
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
